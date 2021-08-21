81c2805184de08130f71bfb9acf60f8ff81b7b98
TomJohnstone08 <88178120+TomJohnstone08@users.noreply.github.com>
2021-08-21 19:28:15 +0930

    Portal, Jump, Gravity, Character, scenery.

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.over(false, effects.melt)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.over(true, effects.starField)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Phil.vy == 0) {
+        Phil.vy = -150
+    }
+})
+let Phil: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+Phil = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . f f f . . . . . . . 
     . . . . . . f 8 f . . . . . . . 
@@ -17,5 +29,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(Phil, 100, 0)
 tiles.setTilemap(tilemap`level1`)
+Phil.ay = 350
+scene.cameraFollowSprite(Phil)
